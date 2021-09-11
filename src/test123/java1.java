65373dbd0ebb753685a6e91f57af405f9709d4f6
tcting <tcting@mitac.com.tw>
2021-09-11 09:25:06 +0800

    practice123

---
+++ src/test123/java1.java
@@ -0,0 +1,60 @@
+package test123;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class java1 {
+	
+
+	public static void main(String[] args) {
+		
+		HashMap<String, Integer> City = new HashMap<String, Integer>();
+		
+        City.put("A", 10);
+        City.put("B", 11);
+        City.put("C", 12);
+        City.put("D", 13);
+        City.put("E", 14);
+        City.put("F", 15);
+        City.put("G", 16);
+        City.put("H", 17);
+        City.put("I", 34);
+        City.put("J", 18);
+        City.put("K", 19);
+        City.put("L", 20);
+        City.put("M", 21);
+        City.put("N", 22);
+        City.put("O", 35);
+        City.put("P", 23);
+        City.put("Q", 24);
+        City.put("R", 25);
+        City.put("S", 26);
+        City.put("T", 27);
+        City.put("U", 28);
+        City.put("V", 29);
+        City.put("X", 30);
+        City.put("Y", 31);
+        City.put("W", 32);
+        City.put("Z", 33);
+        
+        Map<Integer, Integer> MagnificationMap = new HashMap<>();
+		MagnificationMap.put(1, 1);
+		MagnificationMap.put(2, 9);
+		MagnificationMap.put(3, 8);
+		MagnificationMap.put(4, 7);
+		MagnificationMap.put(5, 6);
+		MagnificationMap.put(6, 5);
+		MagnificationMap.put(7, 4);
+		MagnificationMap.put(8, 3);
+		MagnificationMap.put(9, 2);
+		MagnificationMap.put(10, 1);	
+		
+		String str1 = "A100987638";
+		String text = null;
+		Integer fitstNum = str1.get(text.substring(0, 1));
+		
+        System.out.println(i);
+        //System.out.println(City);
+	}
+
+}
